966d0134eb0a5111aa108509eb7fc2953977be05
KUGA16 <gachan_1216@yahoo.co.jp>
2020-02-19 15:07:35 +0000

    [Add]フラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = 'Book was successfully created.'
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = 'Book was successfully created.'
+       redirect_to books_path
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
@@ -20,10 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = 'Book was successfully updated.'
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = 'Book was successfully updated.'
+       redirect_to book_path(book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
